c409b9e337700b7c9593a52f9eb9974de2e3e876
rayd56 <ebelerayd@gmail.com>
2026-02-27 15:09:57 +0100

    notification.js

--- scripts/cmds/notification.js
+++ scripts/cmds/notification.js
@@ -1,43 +1,42 @@
 module.exports = {
-  config: {
-    name: "noti",
-    version: "2.0",
-    author: "Octavio Wina",
-    role: 2, 
-    category: "group",
-    shortDescription: "Notification officielle du supérieur",
-    guide: {
-      fr: "{pn} <message>"
-    }
-  },
-  onStart: async ({ message, args, event, usersData, api }) => {
-    if (!args.length) {
-      return message.reply("❌ Écris le message de notification.");
-    }
-    const content = args.join(" ");
-    const userName = await usersData.getName(event.senderID);
-    const notiMsg = `╭─「 🔔 NOTIFICATION OFFICIELLE 」─╮
+    config: {
+        name: "noti",
+        version: "2.0",
+        author: "Octavio Wina",
+        role: 2, // 2 = Admin bot seulement
+        category: "group",
+        shortDescription: "Notification officielle du supérieur",
+        guide: {
+            fr: "{pn} <message>"
+        }
+    },
+
+    onStart: async ({ message, args, event, usersData }) => {
+        if (!args.length) {
+            return message.reply("❌ Écris le message de notification.");
+        }
+
+        const content = args.join(" ");
+        const userName = await usersData.getName(event.senderID);
+
+        const notiMsg =
+`╭─「 🔔 NOTIFICATION OFFICIELLE 」─╮
 │
 │ ${content}
 │
 ╰────────────────────────────╯
-Message de mon supérieur @${userName}
-il vous dit ${content}
-Utilisée !callad pour me contacté`;
 
-    try {
-      const threadList = await api.getThreadList(10, null, ['INBOX']);
-      threadList.forEach((thread) => {
-        if (thread.isGroup) {
-          api.sendMessage({
+Message de mon supérieur @${userName} il vous dit
+$
+
+Utilisée ✓callad pour me contacté`;
+
+        return message.reply({
             body: notiMsg,
-            mentions: [{ id: event.senderID, tag: `@${userName}` }]
-          }, thread.threadID);
-        }
-      });
-      return message.reply("✅ Notification envoyée dans tous les groupes.");
-    } catch (error) {
-      console.error, message.reply("❌ Une erreur est survenue lors de l'envoi de la notification.");
+            mentions: [{
+                id: event.senderID,
+                tag: `@${userName}`
+            }]
+        });
     }
-  }
 };
